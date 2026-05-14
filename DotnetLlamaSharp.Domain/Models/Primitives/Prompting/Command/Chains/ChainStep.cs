00b2d39df2b0f28da30ab7ebbf9ed7ac8e5cfdc7
AEstradaGrech <alvaro.estrada89@gmail.com>
2026-05-14 23:18:48 +0200

    [lame-chain] --> beginning LameChain. Adding step & step output

---
+++ DotnetLlamaSharp.Domain/Models/Primitives/Prompting/Command/Chains/ChainStep.cs
@@ -0,0 +1,23 @@
+﻿using DotnetLlamaSharp.Domain.Models.Primitives.Prompting.Command.Bases;
+
+
+namespace DotnetLlamaSharp.Domain.Models.Primitives.Prompting.Command.Chains
+{
+    public class ChainStep : IChaineable
+    {
+        private IPromptCommand _command;
+        private IPromptCommand _next;
+        public IPromptCommand Command => _command;
+        public IPromptCommand Next => _next;
+
+        public void Chain(IPromptCommand next)
+        {
+            _next = next;
+        }
+
+        public bool IsChained()
+            => Next != null;
+
+        
+    }
+}
---
+++ DotnetLlamaSharp.Domain/Models/Primitives/Prompting/Command/Chains/StepOutput.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Schema;
+
+namespace DotnetLlamaSharp.Domain.Models.Primitives.Prompting.Command.Chains
+{
+    public class StepOutput<T> where T : class // JsonStructureOutput
+    {
+        public StepOutput(T result)
+        {
+            SerializedResult = JsonSerializer.Serialize<T>(result);
+            JsonSchema = JsonSerializer.Serialize(JsonSerializerOptions.Default.GetJsonSchemaAsNode(typeof(T)));
+        }
+        public string SerializedResult { get; set; }
+        public string JsonSchema { get; set; }
+
+        
+    }
+}
